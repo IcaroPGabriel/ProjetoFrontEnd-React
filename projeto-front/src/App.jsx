b6ef2cfdd94080c2639967dc39e634aa38cd2304
IcaroPGabriel <icaropereiragabriel@outlook.com>
2024-10-27 22:41:53 -0300

    Criando um conceito de checkbox
    Criando um conceito de checkbox

--- projeto-front/src/App.jsx
+++ projeto-front/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DynamicDemo from './componentes/Teste'
+import FilterGroup from './componentes/FilterGroup'
+
 
 
 
@@ -13,7 +16,8 @@ function App() {
       <h1>Testando as bibliotecas do react</h1>
       <article>
         <h2>Realizando testes</h2>
-        
+        <DynamicDemo />
+        <FilterGroup />
         
         
       </article>
--- projeto-front/src/componentes/FilterGroup.jsx
+++ projeto-front/src/componentes/FilterGroup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Checkbox } from "primereact/checkbox";
-import { primeflex } from "/node_modules/primeflex/primeflex.css";
+
 
 function FilterGroup() {
     let categorias = [
@@ -15,23 +15,29 @@ function FilterGroup() {
     function escolherCategoria(x) {
         let _selCategorias = [...selCategorias]
 
-        if (x.checked) {
+        if (x.checked) 
             _selCategorias.push(x.value);
-        } else {
+         else 
             _selCategorias = _selCategorias.filter(categoria => categoria.key !== x.value.key);
             setSelCategorias(_selCategorias);
-        }
+        
     }
-
 
 
     return ( 
         <>
         <article className="card d-flex justify-content-center">
-            <section className="d-flex-colunm">
-                <div>
-
-                </div>
+            <section className="d-flex flex-colunm gap-2">
+                {categorias.map((categoria) => {
+                    return(
+                        <div key={categoria.key} className="d-flex align-itens-center">
+                            <Checkbox inputId={categoria.key} name="categoria" value={categoria} onChange={escolherCategoria} checked={selCategorias.some((caixa) => caixa.key === categoria.key)} />
+                            <label htmlFor={categoria.key} className="ml-3">
+                                {categoria.key}
+                            </label>
+                        </div>
+                    );
+                })}
             </section>
         </article>
         </>
