0392b6427ad9b9abec8d7ed74c4ae17cc177d074
IcaroPGabriel <icaropereiragabriel@outlook.com>
2024-10-30 09:55:42 -0300

    Testando novas propriedades do React
    Testando novas propriedades do React

--- projeto2/src/App.jsx
+++ projeto2/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import FiltroProdutos from './filtro/FiltroProdutos'
 
 
 
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <h1>Segundo protótipo do filtro do projeto</h1>
-      
+      <FiltroProdutos/>
     </>
   )
 }
--- projeto2/src/filtro/FiltroProdutos.jsx
+++ projeto2/src/filtro/FiltroProdutos.jsx
@@ -1,39 +1,18 @@
 
 import React, { useState } from "react";
-import { Checkbox } from "primereact/checkbox";
+import { RadioButton } from "primereact/radiobutton";
+import { Checkbox } from 'primereact/checkbox';
 
-let [cities, setCities] = useState([]);
 
-function FiltroProdutos (e) {
-    let selectedCities = [...cities];
-    if(e.checked)
-        selectedCities.push(e.value);
-    else
-        selectedCities.splice(selectedCities.indexOf(e.value), 1);
 
-    setCities(selectedCities);
+export default function BasicDemo() {
+    const [checked, setChecked] = useState(false);
+
+    return (
+        <div className="card flex justify-content-center">
+            <Checkbox onChange={e => setChecked(e.checked)} checked={checked}></Checkbox>
+            <RadioButton/>
+        </div>
+    )
 }
         
-return(
-    <>
-    <article>
-    <div className="col-12">
-        <Checkbox inputId="cb1" value="New York" onChange={onCityChange} checked={cities.includes('New York')}></Checkbox>
-        <label htmlFor="cb1" className="p-checkbox-label">New York</label>
-    </div>
-    <div className="col-12">
-        <Checkbox inputId="cb2" value="San Francisco" onChange={onCityChange} checked={cities.includes('San Francisco')}></Checkbox>
-        <label htmlFor="cb2" className="p-checkbox-label">San Francisco</label>
-    </div>
-    <div className="col-12">
-        <Checkbox inputId="cb3" value="Los Angeles" onChange={onCityChange} checked={cities.includes('Los Angeles')}></Checkbox>
-        <label htmlFor="cb3" className="p-checkbox-label">Los Angeles</label>
-    </div>
-    </article>
-    </>
-)
-
-
-
-
-export default FiltroProdutos;
