38386e97e493c580eacc9d58d22a0a766286fdfb
IcaroPGabriel <icaropereiragabriel@outlook.com>
2024-11-01 11:16:19 -0300

    Alterando nomes do input
    Alterando nomes do input

--- projeto-front/src/componentes/FilterGroup.jsx
+++ projeto-front/src/componentes/FilterGroup.jsx
@@ -4,26 +4,26 @@ import { Checkbox } from "primereact/checkbox";
 
 function FilterGroup() {
 
-    const marcas = [
-        {name:'Adidas', value:'A'},
-        {name:'Nike', value:'N'},
-        {name:'Puma', value:'P'},
-        {name:'K-Swiss',value:'K'},
-        {name:'Calenciaga', value:'C'}]
+    // const marcas = [
+    //     {name:'Adidas', value:'A'},
+    //     {name:'Nike', value:'N'},
+    //     {name:'Puma', value:'P'},
+    //     {name:'K-Swiss',value:'K'},
+    //     {name:'Calenciaga', value:'C'}]
 
-    const [marcado,SetMarcado] = useState(false);
+    // const [marcado,SetMarcado] = useState(false);
 
-    function Verificado(props) {
+    // function Verificado(props) {
         
-        let {name, checked} = props.target
+    //     let {name, checked} = props.target
 
-        SetMarcado({
+    //     SetMarcado({
             
-        })
+    //     })
 
-        console.log(name);
+    //     console.log(name);
         
-    }
+    // }
 
     
     
@@ -34,49 +34,49 @@ function FilterGroup() {
         <article>
             <h3>Marcas</h3>
             <section className="marca">
-                <input type="checkbox" name="A" id="adidas" checked={marcado} onChange={Verificado} />
+                <input type="checkbox" name="marca" id="adidas" />
                 <label htmlFor="">adidas</label>
                 <br />
-                <input type="checkbox" name="N" id="nike" />
+                <input type="checkbox" name="marca" id="nike" />
                 <label htmlFor="">nike</label>
                 <br />
-                <input type="checkbox" name="P" id="puma" />
+                <input type="checkbox" name="marca" id="puma" />
                 <label htmlFor="">puma</label>
                 <br />
-                <input type="checkbox" name="U" id="umbro" />
+                <input type="checkbox" name="marca" id="umbro" />
                 <label htmlFor="">umbro</label>
             </section>
             <h3>Categorias</h3>
             <section className="categoria">
-                <input type="checkbox" name="esporte-lazer" id="esporte-lazer" />
+                <input type="checkbox" name="categoria" id="esporte-lazer" />
                 <label htmlFor="">esporte e lazer</label>
                 <br />
-                <input type="checkbox" name="casual" id="casual" />
+                <input type="checkbox" name="categoria" id="casual" />
                 <label htmlFor="">casual</label>
                 <br />
-                <input type="checkbox" name="utilitario" id="utilitario" />
+                <input type="checkbox" name="categoria" id="utilitario" />
                 <label htmlFor="">utilitario</label>
                 <br />
-                <input type="checkbox" name="corrida" id="corrida" />
+                <input type="checkbox" name="categoria" id="corrida" />
                 <label htmlFor="">corrida</label>
             </section>
             <h3>Gênero</h3>
             <section className="genero">
-                <input type="checkbox" name="masculino" id="masculino" />
+                <input type="checkbox" name="genero" id="masculino" />
                 <label htmlFor="">masculino</label>
                 <br />
-                <input type="checkbox" name="feminino" id="feminino" />
+                <input type="checkbox" name="genero" id="feminino" />
                 <label htmlFor="">feminino</label>
                 <br />
-                <input type="checkbox" name="unissex" id="unissex" />
+                <input type="checkbox" name="genero" id="unissex" />
                 <label htmlFor="">unissex</label>
             </section>
             <section className="condicao">
                 <h3>Estado</h3>
-                <input type="radio" name="novo" id="novo" />
+                <input type="radio" name="condicao" id="novo" />
                 <label htmlFor="">novo</label>
                 <br />
-                <input type="radio" name="usado" id="usado" />
+                <input type="radio" name="condicao" id="usado" />
                 <label htmlFor="">usado</label>
             </section>
         </article>
