742ebbf569bbabef262e57bd8352429a1499ffff
IcaroPGabriel <icaropereiragabriel@outlook.com>
2024-11-07 12:01:14 -0300

    Atualizando Projeto digital
    Atualizando Projeto digital

--- projeto-front/src/componentes/FilterGroup.jsx
+++ projeto-front/src/componentes/FilterGroup.jsx
@@ -53,60 +53,69 @@ function FilterGroup() {
     
     return ( 
         <>
-        <h1>Filtrar por</h1>
-        <hr />
-        <article>
-            <h3>Marcas</h3>
-            <section className="marcas">
-                <input type="checkbox" name="adidas" id="adidas" checked={marcado.adidas} onChange={Verificado} />
-                <label htmlFor="adidas">{marcas[0].name}</label>
-                <br />
-                <input type="checkbox" name="nike" id="nike" checked={marcado.nike} onChange={Verificado} />
-                <label htmlFor="nike">{marcas[1].name}</label>
-                <br />
-                <input type="checkbox" name="puma" id="puma" checked={marcado.puma} onChange={Verificado} />
-                <label htmlFor="puma">{marcas[2].name}</label>
-                <br />
-                <input type="checkbox" name="kswiss" id="kswiss" checked={marcado.kswiss} onChange={Verificado} />
-                <label htmlFor="kswiss">{marcas[3].name}</label>
-                <br />
-                <input type="checkbox" name="calenciaga" id="calenciaga" checked={marcado.calenciaga} onChange={Verificado} />
-                <label htmlFor="calenciaga">{marcas[4].name}</label>
-            </section>
-            <h3>Categorias</h3>
-            <section className="categoria">
-                <input type="checkbox" name="esporte" id="esporte" checked={marcado.esporte} onChange={Verificado} />
-                <label htmlFor="esporte">{categorias[0].nome}</label>
-                <br />
-                <input type="checkbox" name="casual" id="casual" checked={marcado.casual} onChange={Verificado} />
-                <label htmlFor="casual">{categorias[1].nome}</label>
-                <br />
-                <input type="checkbox" name="utilitario" id="utilitario" checked={marcado.utilitario} onChange={Verificado}/>
-                <label htmlFor="utilitario">{categorias[2].nome}</label>
-                <br />
-                <input type="checkbox" name="corrida" id="corrida" checked={marcado.corrida} onChange={Verificado} />
-                <label htmlFor="corrida">{categorias[3].nome}</label>
-            </section>
-            <h3>Gênero</h3>
-            <section className="genero">
-                <input type="checkbox" name="masculino" id="masculino" checked={marcado.masculino} onChange={Verificado} />
-                <label htmlFor="">{genero[0].valor}</label>
-                <br />
-                <input type="checkbox" name="feminino" id="feminino" checked={marcado.feminino} onChange={Verificado} />
-                <label htmlFor="">{genero[1].valor}</label>
-                <br />
-                <input type="checkbox" name="unissex" id="unissex" checked={marcado.unissex} onChange={Verificado} />
-                <label htmlFor="">{genero[2].valor}</label>
-            </section>
-            <section className="condicao">
-                <h3>Estado</h3>
-                <input type="radio" name="novo" id="novo" checked={marcado.novo} onChange={Verificado} />
-                <label htmlFor="novo">{condicao[0].estado}</label>
-                <br />
-                <input type="radio" name="usado" id="usado" checked={marcado.usado} onChange={Verificado} />
-                <label htmlFor="usado">{condicao[1].estado}</label>
-            </section>
-        </article>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>Filtrar por</h1>
+                    <hr />
+                    <article>
+                        <h3>Marcas</h3>
+                        <section className="marcas">
+                            <input type="checkbox" name="adidas" id="adidas" checked={marcado.adidas} onChange={Verificado} />
+                            <label htmlFor="adidas">{marcas[0].name}</label>
+                            <br />
+                            <input type="checkbox" name="nike" id="nike" checked={marcado.nike} onChange={Verificado} />
+                            <label htmlFor="nike">{marcas[1].name}</label>
+                            <br />
+                            <input type="checkbox" name="puma" id="puma" checked={marcado.puma} onChange={Verificado} />
+                            <label htmlFor="puma">{marcas[2].name}</label>
+                            <br />
+                            <input type="checkbox" name="kswiss" id="kswiss" checked={marcado.kswiss} onChange={Verificado} />
+                            <label htmlFor="kswiss">{marcas[3].name}</label>
+                            <br />
+                            <input type="checkbox" name="calenciaga" id="calenciaga" checked={marcado.calenciaga} onChange={Verificado} />
+                            <label htmlFor="calenciaga">{marcas[4].name}</label>
+                        </section>
+                        <h3>Categorias</h3>
+                        <section className="categoria">
+                            <input type="checkbox" name="esporte" id="esporte" checked={marcado.esporte} onChange={Verificado} />
+                            <label htmlFor="esporte">{categorias[0].nome}</label>
+                            <br />
+                            <input type="checkbox" name="casual" id="casual" checked={marcado.casual} onChange={Verificado} />
+                            <label htmlFor="casual">{categorias[1].nome}</label>
+                            <br />
+                            <input type="checkbox" name="utilitario" id="utilitario" checked={marcado.utilitario} onChange={Verificado}/>
+                            <label htmlFor="utilitario">{categorias[2].nome}</label>
+                            <br />
+                            <input type="checkbox" name="corrida" id="corrida" checked={marcado.corrida} onChange={Verificado} />
+                            <label htmlFor="corrida">{categorias[3].nome}</label>
+                        </section>
+                        <h3>Gênero</h3>
+                        <section className="genero">
+                            <input type="checkbox" name="masculino" id="masculino" checked={marcado.masculino} onChange={Verificado} />
+                            <label htmlFor="">{genero[0].valor}</label>
+                            <br />
+                            <input type="checkbox" name="feminino" id="feminino" checked={marcado.feminino} onChange={Verificado} />
+                            <label htmlFor="">{genero[1].valor}</label>
+                            <br />
+                            <input type="checkbox" name="unissex" id="unissex" checked={marcado.unissex} onChange={Verificado} />
+                            <label htmlFor="">{genero[2].valor}</label>
+                        </section>
+                        <section className="condicao">
+                            <h3>Estado</h3>
+                            <input type="radio" name="novo" id="novo" checked={marcado.novo} onChange={Verificado} />
+                            <label htmlFor="novo">{condicao[0].estado}</label>
+                            <br />
+                            <input type="radio" name="usado" id="usado" checked={marcado.usado} onChange={Verificado} />
+                            <label htmlFor="usado">{condicao[1].estado}</label>
+                        </section>
+                    </article>
+                    <div className="col">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
      );
 }
